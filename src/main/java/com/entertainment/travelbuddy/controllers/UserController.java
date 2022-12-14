2b8a5a755e737262e0cdd3c969405af2632b9458
Boryana1 <boryana.dimova1@gmail.com>
2022-12-14 05:49:13 -0600

    Created the UserController.

--- src/main/java/com/entertainment/travelbuddy/controllers/UserController.java
+++ src/main/java/com/entertainment/travelbuddy/controllers/UserController.java
@@ -1,2 +1,14 @@
-package com.entertainment.travelbuddy.controllers;public class UserController {
+package com.entertainment.travelbuddy.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserController {
+
+//    Create User Account
+    @GetMapping("/sign-up")
+    public String signUp(Model model){
+        model.addAllAttributes("user", new User());
+        return "user/register"
+    }
 }
