66d425197913debd1128eebe62570a18d57b6dd9
Boryana1 <boryana.dimova1@gmail.com>
2022-12-31 14:34:13 -0600

    Created an UserRepository

--- src/main/java/com/entertainment/travelbuddy/repositories/UserRepository.java
+++ src/main/java/com/entertainment/travelbuddy/repositories/UserRepository.java
@@ -1,2 +1,12 @@
-package com.entertainment.travelbuddy.repositories;public interface UserRepository {
+package com.entertainment.travelbuddy.repositories;
+
+import com.entertainment.travelbuddy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
 }
